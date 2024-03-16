274bdb4f353aaa67b291efd21265f4c857d1a678
= <joseph.bradshaw@outlook.com>
2024-03-16 02:50:50 +0100

    Prevent escape char

--- src/app/components/Intro/Intro.tsx
+++ src/app/components/Intro/Intro.tsx
@@ -3,7 +3,7 @@ import styles from "./Intro.module.css";
 export default function Intro() {
   return (
     <div className={styles.introContainer}>
-      <div className={styles.introText}>I'm</div>
+      <div className={styles.introText}>{"I'm"}</div>
       <div className={styles.textContainer}>
         <ul className={styles.list}>
           <li className={`${styles.introText} ${styles.blue}`}>Joseph</li>
