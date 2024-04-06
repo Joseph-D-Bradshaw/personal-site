cf7b798ed0ec4070299a5ba08b54a08ad8c039d9
= <joseph.bradshaw@outlook.com>
2024-04-06 05:13:06 +0200

    Make MeAndPoco component more responsive with less CSS and drop css module in favour of regular css file

--- src/app/components/MeAndPoco/MeAndPoco.tsx
+++ src/app/components/MeAndPoco/MeAndPoco.tsx
@@ -1,25 +1,26 @@
 import Image from "next/image";
 import Me from "../../../../public/me.png";
 import Poco from "../../../../public/poco.png";
-import styles from "./MeAndPoco.module.css";
+import "./MeAndPoco.css";
 
 export default function MeAndPoco() {
   return (
-    <div className={styles.meContainer}>
-      <div className={styles.imageContainer}>
-        <Image className={`${styles.me} ${styles.meImage}`} src={Me} alt="Hello, it's an AI generated me!" />
-        <div className={styles.poco}>
-          <Image className={` ${styles.pocoImage}`} src={Poco} alt="An AI generated version of my dog Poco" />
-          <div className={styles.flex}>
-            <div className={styles.pocosName}>Poco</div>
-            <div className={styles.pocosDesc}>
-              <span className={styles.nowrap}>Professional Attention</span> Seeker
+    <div className="joseph-and-poco">
+      <div className="image-container">
+        <Image className="image-container__joseph" layout="responsive" src={Me} alt="Hello, it's an AI generated me!" />
+        <div className="image-container__poco">
+          <Image className="image-container__poco__image" layout="responsive" src={Poco} alt="My dog Poco" />
+          <div className="flex">
+            <div className="image-container__poco__name">Poco</div>
+            <div className="image-container__poco__desc">
+              <span className="nowrap">Professional Attention</span> Seeker
             </div>
           </div>
         </div>
       </div>
-      <div className={`${styles.myName} ${styles.linearGradient}`}>Joseph Bradshaw</div>
-      <div className={styles.myDesc}>Senior Full-Stack Engineer & Computer Scientist</div>
+
+      <div className="image-container__joseph__name nowrap linear-gradient">Joseph Bradshaw</div>
+      <div className="image-container__joseph__desc nowrap">Senior Full-Stack Engineer & Computer Scientist</div>
     </div>
   );
 }
